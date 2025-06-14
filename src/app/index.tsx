ecfff1a1cd12a95db63bd57a800a96ff0fd1b8a6
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-06-13 23:57:41 -0300

    limpeza de arquivos

---
+++ src/app/index.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}> Olá mundo!!!</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'red',
+    }
+})
