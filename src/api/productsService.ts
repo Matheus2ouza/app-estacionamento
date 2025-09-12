92bf19ed3102973d6895f18e7738b242cd2319cc
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-09-12 06:39:39 -0300

    Adiciona funções para busca de produtos pelo código de barras utilizando UPCitemDB e OpenFoodFacts, além de atualizar a função de criação de produtos para retornar a nova interface ProductRegisterResponse, melhorando a integração com APIs externas.

--- src/api/productsService.ts
+++ src/api/productsService.ts
@@ -1,4 +1,5 @@
-import { Product, Responselist } from "../types/productsTypes/products";
+import axios from "axios"; // usamos axios puro para as APIs externas
+import { Product, ProductRegisterResponse, Responselist } from "../types/productsTypes/products";
 import axiosInstance from "./axiosInstance";
 
 export const ProductApi = {
@@ -11,8 +12,32 @@ export const ProductApi = {
     return response.data;
   },
 
-  createProduct: async (data: Product): Promise<Response> => {
-    const response =  await axiosInstance.post('/products/create', data);
-    return response.data
+  createProduct: async (data: Product): Promise<ProductRegisterResponse> => {
+    const response = await axiosInstance.post('/products/create', data);
+    return response.data;
+  },
+
+  // 🔎 Busca produto pelo código de barras (UPC/EAN) usando UPCitemDB
+  lookupByBarcodeUPC: async (upc: string): Promise<any> => {
+    const response = await axios.post(
+      "https://api.upcitemdb.com/prod/trial/lookup",
+      { upc },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+          "Accept-Encoding": "gzip, deflate",
+        },
+      }
+    );
+    return response.data;
+  },
+
+  // 🍎 Busca produto pelo código de barras usando OpenFoodFacts
+  lookupByBarcodeOFF: async (barcode: string): Promise<any> => {
+    const response = await axios.get(
+      `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`
+    );
+    return response.data;
   }
 }
