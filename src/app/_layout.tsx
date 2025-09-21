96a4ac062fc88e14d97cac94c4b8459064374769
Matheus2ouza <matheusfurtadogg@gmail.com>
2025-09-20 21:28:35 -0300

    Adiciona suporte para simulação de notificações no Android e implementa lógica para exibição de banners de notificações no aplicativo. Atualiza o arquivo eas.json para incluir configurações de simulação para Android.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,9 +3,10 @@ import { AuthProvider } from "@/context/AuthContext";
 import { CashProvider } from "@/context/CashContext";
 import { ProductCacheProvider } from "@/context/ProductCacheContext";
 import { useFonts } from "expo-font";
+import * as Notifications from "expo-notifications";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { View } from "react-native";
 import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
 
@@ -19,8 +20,37 @@ const theme = {
   },
 };
 
+
+// Configura para mostrar banner mesmo com app aberto
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowBanner: true,  // banner no topo (foreground)
+    shouldShowList: true,    // adiciona na lista de notificações
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
 export default function RootLayout() {
   const [fontsLoaded] = useFonts(FontsToLoad);
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(async (notification) => {
+      // ⚡️ Apenas dispara a mesma notificação como local
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: notification.request.content.title,
+          body: notification.request.content.body,
+          data: notification.request.content.data,
+          sound: "default",
+        },
+        trigger: null, // mostra imediatamente
+      });
+    });
+
+    return () => subscription.remove();
+  }, []);
+  
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
